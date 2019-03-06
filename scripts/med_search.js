9ca61f4fe01d175e06c02a0693d5819229e49e69
dg-7998 <deepsachin98@gmail.com>
2019-03-06 12:42:40 +0530

    Update med_search.js

--- scripts/med_search.js
+++ scripts/med_search.js
@@ -1,3 +1,11 @@
 function search() {
 	var medicine_name = document.getElementById('med_name');
+	var database = firebase.database();
+	if((medicine_name.value).equalTo())
+		ref.child('users').orderByChild('name').equalTo('John Doe').on("value", function(snapshot) {
+    console.log(snapshot.val());
+    snapshot.forEach(function(data) {
+        console.log(data.key);
+    });
+});
 }
