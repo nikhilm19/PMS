fa9f449f4a4921d5f2720e454c7bb8295738c58e
dg-7998 <deepsachin98@gmail.com>
2019-03-11 19:06:13 +0530

    Add files via upload

--- scripts/med_search.js
+++ scripts/med_search.js
@@ -1,11 +1,30 @@
-function search() {
-	var medicine_name = document.getElementById('med_name');
-	var database = firebase.database();
-	if((medicine_name.value).equalTo())
-		ref.child('users').orderByChild('name').equalTo('John Doe').on("value", function(snapshot) {
-    console.log(snapshot.val());
-    snapshot.forEach(function(data) {
-        console.log(data.key);
-    });
-});
-}
+function search() 
+{
+	var medicine_name = document.getElementById('med_name').value;
+	var query = firebase.database().ref("medicines").orderByKey();
+	query.once("value").then(function(snapshot){
+		snapshot.forEach(function(childSnapshot)
+		{
+			var  key = childSnapshot.key;
+			//var childData = childSnapshot.val();
+			var name=childSnapshot.child("name").val();
+			var name1=childSnapshot.child("name").val();
+			name = name.split(" ");
+			name = name[0];
+			if(name==medicine_name && name1.includes(name))
+			{
+				var id=childSnapshot.child("medicine_id").val();
+				var query1 = firebase.database().ref("medicines/"+id).orderByKey();
+				query1.once("value").then(function(snapshot)
+				{
+					snapshot.forEach(function(childSnapshot)
+					{
+						var  key = childSnapshot.key;
+						var childData = childSnapshot.val();
+						console.log(childData);
+					});
+				});
+			}
+		});
+	});
+}	
