4bf6d1c829cc1514140d41c8a1c88755af59184e
Nikhil Mulchandani <nikmul19@gmail.com>
2019-03-17 18:55:02 +0530

    Add files via upload

--- scripts/login.js
+++ scripts/login.js
@@ -1,20 +1,24 @@
 function login(){
+
+
 	var email=document.getElementById("email").value;
 	var password=document.getElementById("pass").value;
-	console.log(email);
 	//alert(email+" " + password);
-	
-	firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
+  firebase.auth().signOut();
+
+  firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
 		 var errorCode = error.code;
 		var errorMessage = error.message;
 		
 		alert(errorMessage);
 });
+}
 
-firebase.auth().onAuthStateChanged(function(user) {
+function initApp(){
+  firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    window.location="/home/pc001/Desktop/PMS-master (1)/PMS-master/home.html"
+    //window.location="/home/pc001/Desktop/PMS-master (1)/PMS-master/home.html"
     var displayName = user.displayName;
     var email = user.email;
     var emailVerified = user.emailVerified;
@@ -22,11 +26,16 @@ firebase.auth().onAuthStateChanged(function(user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     var providerData = user.providerData;
-    //alert(email);
+    alert("Signed in succesfully");
   } 
   else {
-    alert("Incorrect Credentials");
+    //alert("Incorrect Credentials");
   }
 });
+
+}
+window.onload=function(){
+
+  initApp();
 }
 	
