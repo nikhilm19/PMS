f3c14825c0cf5ae5ed291c6d27d6a535c8d69bbe
dg-7998 <deepsachin98@gmail.com>
2019-03-17 15:54:34 +0530

    Add files via upload

--- scripts/login.js
+++ scripts/login.js
@@ -1,20 +1,20 @@
 function login(){
 	var email=document.getElementById("email").value;
 	var password=document.getElementById("pass").value;
-	alert(email+" " + password);
+	console.log(email);
+	//alert(email+" " + password);
 	
 	firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
 		 var errorCode = error.code;
 		var errorMessage = error.message;
 		
 		alert(errorMessage);
+});
 
-		
-		
-		});
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
+    window.location="/home/pc001/Desktop/PMS-master (1)/PMS-master/home.html"
     var displayName = user.displayName;
     var email = user.email;
     var emailVerified = user.emailVerified;
@@ -22,8 +22,9 @@ firebase.auth().onAuthStateChanged(function(user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     var providerData = user.providerData;
-    alert(email);
-  } else {
+    //alert(email);
+  } 
+  else {
     alert("Incorrect Credentials");
   }
 });
